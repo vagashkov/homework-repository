76bbb7f58a58ba4ba165aac42295717de527f5b2
vagashkov <vitali.agashkov@gmail.com>
2021-11-08 16:32:43 +0300

    PEP E127 in task4.py fixed

--- homework1/task4.py
+++ homework1/task4.py
@@ -1,7 +1,8 @@
 from typing import List
 
 
-def check_sum_of_four(a: List[int], 
+def check_sum_of_four(
+                                      a: List[int],
                                       b: List[int],
                                       c: List[int],
                                       d: List[int]) -> int:
