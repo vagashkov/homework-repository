5df5af80a313dd3169783b4c42b16a87b195896a
vagashkov <vitali.agashkov@gmail.com>
2021-11-18 12:41:29 +0300

    test_task1 PEP violations fixed

--- tests/homework3/test_task1.py
+++ tests/homework3/test_task1.py
@@ -8,11 +8,11 @@ monkeypatch = MonkeyPatch()
 test_inputs = StringIO('12\n')
 monkeypatch.setattr('sys.stdin', test_inputs)
 
+
 def test_timed_lru_cache(monkeypatch):
     def f():
         print("f called")
         return input('? ')
-  
 
     # building cached function
     cached = timed_lru_cache(f, 2)
