6947623699656ece3d3f9d5e60863ddad610e62b
vagashkov <vitali.agashkov@gmail.com>
2021-11-21 20:53:51 +0300

    HW4 initial release (without task5)

---
+++ tests/homework4/test_task1.py
@@ -0,0 +1,40 @@
+﻿import os
+import tempfile
+
+from homework4.task1 import read_magic_number
+
+
+def test_read_magic_number_pos():
+    """Existing file with normal content"""
+    fp = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
+    fp.write('2')
+    fp.close()
+    result = read_magic_number(fp.name)
+    os.remove(fp.name)
+    assert result
+
+
+def test_read_magic_number_neg():
+    """Existing file with out-of-bounds content"""
+    fp = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
+    fp.write('-1')
+    fp.close()
+    result = read_magic_number(fp.name)
+    os.remove(fp.name)
+    assert not result
+
+
+def test_read_magic_number_not_number():
+    """Existing file with abnormal content"""
+    fp = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
+    fp.write('ddd')
+    fp.close()
+    result = read_magic_number(fp.name)
+    os.remove(fp.name)
+    assert not result
+
+
+def test_read_magic_number_nofile():
+    """Non-existing file"""
+    result = read_magic_number('1233')
+    assert not result
