7f76195148110a6d3716e79bcb182fde8048c16e
vagashkov <vitali.agashkov@gmail.com>
2022-01-02 18:35:51 +0300

    HW1 Task4 rewritten using itertools.product, added parametrized test

--- homework1/task4.py
+++ homework1/task4.py
@@ -5,6 +5,7 @@ Given four lists A, B, C, D of integer values,
     A[i] + B[j] + C[k] + D[l] is zero.
 We guarantee, that all A, B, C, D have same length of N where 0 ? N ? 1000.
 """
+from itertools import product
 from typing import List
 
 
@@ -13,15 +14,18 @@ def check_sum_of_four(
                                       b: List[int],
                                       c: List[int],
                                       d: List[int]) -> int:
-    # trying to explore all the
-    # posible combinations
-    # among four lists
+    """ function computes how many element
+    combination of four lists have sum of zero """
+    # trying to explore all the posible combinations
+    # among four lists using itertools.product
+    # function
     counter = 0
-    for item_a in a:
-        for item_b in b:
-            for item_c in c:
-                for item_d in d:
-                    # got the zero sum combo
-                    if item_a + item_b + item_c + item_d == 0:
-                        counter = counter + 1
+    for (item_a,
+            item_b,
+            item_c,
+            item_d) in product(a, b, c, d):
+        # checking if sum of elements is equal to 0
+        if item_a + item_b + item_c + item_d == 0:
+            # and increment counter if necessary
+            counter = counter + 1
     return counter
