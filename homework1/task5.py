e06bdddbbc9d39a41978d23da1e7df64e9858f66
vagashkov <vitali.agashkov@gmail.com>
2022-01-02 20:30:56 +0300

    HW1 Task5 error fixed, added parametrized tests

--- homework1/task5.py
+++ homework1/task5.py
@@ -11,14 +11,17 @@ from typing import List
 
 def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
     # setting initial 'max' value
-    max = sum(nums[:k])
-    counter = 0
+    max_sum = max(nums)
     # browse through the sequence
-    while counter < len(nums) - k + 1:
-        # some k-member aequence sum
-        # is greater than prevous maximum -
-        # so redefine it
-        if sum(nums[counter:counter + k]) > max:
-            max = sum(nums[counter:counter + k])
-        counter = counter + 1
-    return max
+    # starting from the most narrow (length = 1)
+    # to the most wide (length = k) window
+    for window_len in range(1, k+1):
+        counter = 0
+        while counter < len(nums) - window_len + 1:
+            # some "window_len"-member sequence sum
+            # is greater than prevous maximum -
+            # so redefine it
+            if sum(nums[counter:counter + window_len]) > max_sum:
+                max_sum = sum(nums[counter:counter + k])
+            counter = counter + 1
+    return max_sum
