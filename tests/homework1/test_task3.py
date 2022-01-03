85f93bf49484c47a40cff958a5ac8f506e988c96
vagashkov <vitali.agashkov@gmail.com>
2022-01-03 12:26:57 +0300

    HW2 Task1 get_longest_diverse_words function refactored, Unicode symbols output fixed

--- tests/homework1/test_task3.py
+++
@@ -1,6 +0,0 @@
-from homework1.task3 import find_maximum_and_minimum
-
-
-def test_normal_file():
-    """Testing that normal file gives us normal result"""
-    assert find_maximum_and_minimum("/homework1/numbers.txt") == (111, -45)
--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -7,34 +7,41 @@ def test_get_longest_diverse_words():
     """ Find 10 longest words consisting from largest amount
     of unique symbols """
     expected_result = [
-                       'Bev\\u00f6lkerungsabschub,',
-                       'unmi\\u00dfverst\\u00e4ndliche',
-                       'Werkst\\u00e4ttenlandschaft',
-                       'Machtbewu\\u00dftsein,',
-                       'Selbstverst\\u00e4ndlich',
-                       'Entz\\u00fcndbarkeit.',
-                       'r\\u00e9sistance-Bewegungen,',
-                       'unmi\\u00dfverst\\u00e4ndliche',
-                       'Werkst\\u00e4ttenlandschaft',
-                       'Machtbewu\\u00dftsein,']
-    assert get_longest_diverse_words('/homework2/data.txt') == expected_result
+                       u"unmi\u00dfverst\u00e4ndliche",
+                       u"Bev\u00f6lkerungsabschub",
+                       u"Kollektivschuldiger",
+                       u"Werkst\u00e4ttenlandschaft",
+                       u"Schicksalsfiguren",
+                       u"Selbstverst\u00e4ndlich",
+                       u"Fingerabdrucks",
+                       u"Friedensabstimmung",
+                       u"au\u00dfenpolitisch",
+                       u"Seinsverdichtungen"]
+    assert (get_longest_diverse_words('/homework2/data.txt',
+                                      encoding="unicode-escape")
+            == expected_result)
 
 
 def test_get_rarest_char():
     """ Find rarest symbol for document """
-    assert get_rarest_char('/homework2/data.txt') == "Y"
+    assert get_rarest_char('/homework2/data.txt',
+                           encoding="unicode-escape") == u"\u203a"
 
 
 def test_count_punctuation_chars():
     """ Count every punctuation char """
-    assert count_punctuation_chars('/homework2/data.txt') == 43180
+    assert count_punctuation_chars('/homework2/data.txt',
+                                   encoding="unicode-escape") == 40208
 
 
 def test_count_non_ascii_chars():
     """ Count every non ascii char """
-    assert count_non_ascii_chars('/homework2/data.txt') == 2972
+    assert count_non_ascii_chars('/homework2/data.txt',
+                                 encoding="unicode-escape") == 2972
 
 
 def test_get_most_common_non_ascii_char():
     """ Find most common non ascii char for document """
-    assert get_most_common_non_ascii_char('/homework2/data.txt') == '\u00E4'
+    assert (get_most_common_non_ascii_char('/homework2/data.txt',
+                                           encoding="unicode-escape")
+            == '\u00E4')
