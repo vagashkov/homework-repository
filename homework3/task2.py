27e1a2d7ca65d7286d8bb026570772f0ada15bb7
vagashkov <vitali.agashkov@gmail.com>
2022-01-04 10:43:16 +0300

    HW3 both Task2 and Task2 test updated

--- homework3/task2.py
+++ homework3/task2.py
@@ -13,14 +13,11 @@ def slow_calculate(value):
 
 
 def make_calculate():
-    # start 25 worker processes
-    with Pool(processes=25) as pool:
+    # start 50 worker processes
+    with Pool(processes=50) as pool:
         result_list = pool.map(slow_calculate, range(500))
     return sum(result_list)
 
 
 if __name__ == '__main__':
-    # start 25 worker processes
-    with Pool(processes=25) as pool:
-        result_list = pool.map(slow_calculate, range(500))
-    print(sum(result_list))
+    make_calculate()
