3cc6c32d14a261303be3c1714b486c74704c7688
vagashkov <vitali.agashkov@gmail.com>
2022-01-13 11:26:25 +0300

    HW9 Task1 updated

--- homework9/task1.py
+++ homework9/task1.py
@@ -17,15 +17,25 @@ from pathlib import Path
 from typing import Iterator, List, Union
 
 
+def value_generator(file_name):
+    """ generator function reads file line-by-line
+    and returns line content as int value (if it contains any)"""
+    with open(os.getcwd()+"//"+file_name, "r",
+              encoding='utf-8-sig') as file:
+        for line in file:
+            # checking if it is really an int
+            if line.strip().isdigit():
+                yield int(line.strip())
+
+
 def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
-    """ parsing through the files, constructing list line by line"""
-    result = []
+    """ parsing through the files, constructing list and sorting it """
+    all_numbers = []
+    # building number list from all files
+    # we can use nested list comprehension
+    # but for loop maybe more readable
     for filename in file_list:
-        with open(os.getcwd()+"//"+filename, "r",
-                  encoding='utf-8-sig') as file:
-            for line in file:
-                # checking if it is really an int
-                if line.strip().isdigit():
-                    result.append(int(line.strip()))
+        file_numbers = [num for num in value_generator(filename)]
+        all_numbers += file_numbers
     # sorting result and returning iterator object
-    return iter(sorted(result))
+    return iter(sorted(all_numbers))
